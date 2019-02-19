203fde743b8944903eae272517cf451fb4109625
David Lineman <dline215@gmail.com>
2019-02-19 21:43:45 +0000

    added the user model and ran the migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "David", password: "password")
+User.create(username: "Doug", password: "password")
+User.create(username: "Joanie", password: "password")
+User.create(username: "Bobby", password: "password")
+User.create(username: "Keith", password: "password")
